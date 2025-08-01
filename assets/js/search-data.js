e01c96b55d3640758121a2bcf431373a877504c6
zhypatiap <71024809+zhypatiap@users.noreply.github.com>
2025-08-01 02:16:44 +0000

    Deploying to gh-pages from @ zhypatiap/zhypatiap.github.io@d8dbd18f6aa98988eb54fe4c99022ec61c9bb8db 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,18 +23,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2.html";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{
+        },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
