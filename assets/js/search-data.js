4df2cf84eec829a436182d3f09d839b6dc724b9c
zhypatiap <71024809+zhypatiap@users.noreply.github.com>
2025-08-18 16:35:12 +0000

    Deploying to gh-pages from @ zhypatiap/zhypatiap.github.io@b902b0932a57bf6f677c0a309c23ae2c83864d1b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "Download PDF for most recent CV. Updated July 2025.",
+          description: "Download PDF for most recent CV. Updated August 2025.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
